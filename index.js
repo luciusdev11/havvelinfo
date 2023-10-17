173af1d63aa556ff6e647336c71769f713ddeb3e
luciusdev11 <105908121+luciusdev11@users.noreply.github.com>
2023-10-17 22:10:10 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>HAVVEL (Dương quá)</title>
+</head>
+
+<body>
+  <div class="wp-block-cover alignfull" style="min-height:100vh">
+    <span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim"
+      style="background-color:#011a0b"></span>
+    <div class="wp-block-cover__inner-container">
+      <div class="wp-block-group has-text-color has-link-color" style="color:#b3bab6">
+        <div class="wp-block-group">
+          <h2 class="wp-block-heading has-text-color has-space-mono-font-family"
+            style="color:#dbff00;font-size:40px;letter-spacing:0px;text-decoration:none;text-transform:uppercase">HAVVEL
+            (Dương quá)</h2>
+        </div>
+      </div>
+      <div class="wp-block-columns" style="margin-top:50px;margin-bottom:50px">
+        <div class="wp-block-column" style="flex-basis:50%">
+          <p class="has-small-font-size">My SOCIALS</p>
+          <div class="wp-block-group">
+            <p><a href="https://facebook.com/7cloud.lucius" data-type="link"
+                data-id="https://facebook.com/7cloud.lucius">Facebook</a></p>
+            <hr class="wp-block-separator is-style-wide">
+            <p><a href="https://discord.gg/homieesofun">Discord</a></p>
+            <hr class="wp-block-separator is-style-wide">
+            <p><a href="https://open.spotify.com/user/313kww6mo2qqbvvpflkj3vlqykei">Spotify</a></p>
+            <hr class="wp-block-separator is-style-wide">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="script.js"></script>
+</body>
+
+</html>
